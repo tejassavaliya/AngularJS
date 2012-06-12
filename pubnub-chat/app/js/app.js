22411599a5649e7fcd9dd14ff221f155d5fbd1d3
Nolan Dubeau <nolan@guard.ly>
2012-06-12 11:38:15 -0400

    added common library folder
    -added common library folder to house bootstrap and angularjs
    -pubnub demo code

--- pubnub-chat/app/js/app.js
+++ pubnub-chat/app/js/app.js
@@ -1,10 +1,13 @@
-'use strict';
-
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+var PubNubChat = angular.module('PubNubChat',[]).directive('coolFade', function() {
+    return {
+      compile: function(elm) {
+        //console.log('compiling');
+        $(elm).css('opacity', 0);
+        return function(scope, elm, attrs) {
+         // console.log('animating');
+          $(elm).animate({ opacity : 1.0 }, 1000 );
+        };
+      }
+    };
+  });
--- pubnub-chat/app/js/controllers.js
+++ pubnub-chat/app/js/controllers.js
@@ -1,12 +1,77 @@
-'use strict';
+function MyCtrl($scope) {
+    $scope.messages = [];
+    $scope.realtimeStatus = "Connecting...";
+    $scope.channel = "pubnub_chat";
+    $scope.limit = 20;
 
-/* Controllers */
+    //publish a chat message
+    $scope.publish = function() {
+
+        //toggle the progress bar
+        $('#progress_bar').slideToggle();
+
+        PUBNUB.publish({
+            channel: $scope.channel,
+            message: $scope.message
+        });
+
+        //reset the message text
+        $scope.message.text = "";
+    };
+
+    //gets the messages history   
+    $scope.history = function() {
+        PUBNUB.history({
+            channel: $scope.channel,
+            limit: $scope.limit
+        }, function(messages) {
+            // Shows All Messages
+            $scope.$apply(function() {
+                $scope.messages = messages.reverse();
+
+            });
+        });
+    };
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+    //we'll leave these ones as is so that pubnub can
+    //automagically trigger the events
+    PUBNUB.subscribe({
+        channel: $scope.channel,
+        restore: false,
 
+        callback: function(message) {
 
-function MyCtrl2() {
+            //toggle the progress_bar
+            $('#progress_bar').slideToggle();
+
+            $scope.$apply(function() {
+                $scope.messages.unshift(message);
+
+            });
+        },
+
+        disconnect: function() {
+            $scope.$apply(function() {
+                $scope.realtimeStatus = 'Disconnected';
+            });
+        },
+
+        reconnect: function() {
+            $scope.$apply(function() {
+                $scope.realtimeStatus = 'Connected';
+            });
+        },
+
+        connect: function() {
+            $scope.$apply(function() {
+                $scope.realtimeStatus = 'Connected';
+                //hide the progress bar
+                $('#progress_bar').slideToggle();
+                //load the message history from PubNub
+                $scope.history();
+            });
+
+        }
+    });
 }
-MyCtrl2.$inject = [];
